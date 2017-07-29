cbed0edf3111b18e2b52fb1e22bcdf9ac4f7e79b
ashok <ashokjain001@gmail.com>
2017-07-28 22:24:32 -0500

    filerename updates

--- filerename.py
+++ filerename.py
@@ -2,9 +2,9 @@ import os
 def filename():
 
     file_list = os.listdir(r"/Users/ashokjain/Desktop/prank")
-
+    print "file names %s" %file_list
     saved_path = os.getcwd()
-
+    print "current working directory" +saved_path
     os.chdir("/Users/ashokjain/Desktop/prank")
 
     for file_name in file_list:
