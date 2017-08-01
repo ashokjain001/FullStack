11245da86e3466b1c0a95d6f43541af01b53d871
ashok <ashokjain001@gmail.com>
2017-08-01 17:05:12 -0500

    twiio test

---
+++ send_text.py
@@ -0,0 +1,19 @@
+from twilio.rest import TwilioRestClient
+
+# Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC7d50c922529f8e1535bc4c6031b6c70f"
+auth_token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+client = TwilioRestClient(account_sid,auth_token)
+
+message = client.messages.create(
+        to="+14699997067",
+        from_="+18176011349",
+        body="Test Message from Twiio - This is Ron Burgundy")
+print(message.sid)
+
+#call = client.calls.create(url="http://demo.twilio.com/docs/voice.xml",
+#                           to="+15005550003",
+#                           from_="+15005550006")
+#print(Call.sid)
+
