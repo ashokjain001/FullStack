662836313f1561b8410d3f9dc2e5d47f8b1b70fa
ashok <ashokjain001@gmail.com>
2017-08-01 08:11:24 -0500

    update

--- profanityEditor.py
+++ profanityEditor.py
@@ -1,15 +1,16 @@
 # This program will check for profanity in your text document if there is it will print "Profanity alert" and "document
 # has no profanity" if there is no profanity. It reads the content of the file and checks against the website "http://www.wdylike.appspot.com/?q="
 
-import urllib
+from urllib import urlopen
 
 def profanity():
     texts =  open("/Users/ashokjain/Desktop/Python/movie_quotes.txt","r")
     file_contents = texts.read()
+    print file_contents
     check_profanity(file_contents)
     texts.close()
 def check_profanity(text_to_check):
-    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q=" +text_to_check)
+    connection = urlopen("http://www.wdylike.appspot.com/?q=" +text_to_check)
     output = connection.read()
     connection.close()
     if(output) == True:
